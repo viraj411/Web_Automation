8d9dfc351d004db6f90d26ef6aa668e4831b356d
virajabhang <virajabhang18@gmail.com>
2025-01-15 22:42:05 +0530

    Addded New opps

--- src/main/java/JavaPrograming/Fibonacci.java
+++ src/main/java/JavaPrograming/Fibonacci.java
@@ -6,11 +6,5 @@ public class Fibonacci {
             int n = 10; // Example to find the 10th Fibonacci number
             int a = 0, b = 1;
 
-            for (int i = 0; i < n; i++) {
-                System.out.print(a + " ");
-                int temp = ;
-                a = b;
-                b = temp + b;
-            }
         }
     }
